83f9df0dd12a55c2a8358b96d49acf9fa912f6d3
meftuhzz <wahibzz95@gmial.com>
2020-08-30 11:36:43 -0500

    2 level branxh

---
+++ planb-1 - Copy - Copy/src/main/java/com/infosys/infytel/plan/Planb1Application.java
@@ -0,0 +1,13 @@
+package com.infosys.infytel.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Planb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Planb1Application.class, args);
+	}
+
+}
